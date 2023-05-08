bafc5a27666042a1ba1c97a52a023f0c0b46a483
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2023-05-08 13:11:55 +0530

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -2,7 +2,7 @@
 #include<string>
 using namespace std;
 
-class Dictionay{
+class Dictionary{
     public:
     struct Node{
         string keyword;
@@ -14,7 +14,7 @@ class Dictionay{
 
     Node* root;
 
-    Dictionay()
+    Dictionary()
     {
         root=NULL;
     }
@@ -83,14 +83,65 @@ class Dictionay{
             displayTree(node->right);
         }
     }
+
+    Node* copy(Node* node)
+    {
+        if(node==NULL)
+        {
+            return NULL;
+        }
+        Node* newnode=new Node(node->keyword,node->meaning);
+        newnode->left=copy(node->left);
+        newnode->right=copy(node->right);
+        return newnode;
+
+    }
+
+    void mirror(Node* node)
+    {
+        if(node==NULL)
+        {
+            return;
+        }
+
+        mirror(node->left);
+        mirror(node->right);
+
+        Node* temp=node->left;
+        node->left=node->right;
+        node->right=temp;
+
+    }
+
+    
 };
+
 
 int main()
 {
-    Dictionay dict;
+    Dictionary dict;
     dict.insert("Pringles","They are potato chips");
     dict.insert("Apple","A fruit wow");
     dict.insert("Charger","An electronic device");
+    
+
+    // display both dictionaries to verify that they are independent
+    cout << "Original dictionary:" << endl;
     dict.display();
+    cout << endl;
+
+    Dictionary dictCopy(dict);
+    dict.insert("Orange","Another fruit");
+    dict.insert("Laptop","A portable computer");
+
+    cout << "Copy of dictionary:" << endl;
+    dictCopy.display();
+    cout << endl;
+
+    dict.mirror(dict.root);
+    cout << "Mirrored dictionary:" << endl;
+    dict.display();
+    cout << endl;
+
     return 0;
 }
